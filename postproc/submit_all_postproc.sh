a96bedeccab52c130d36a90e0491fa7a2a2b88bc
Ibaoune <mohammad.trop1@gmail.com>
2026-05-17 10:02:05 +0100

    Update postprocessing scripts and configs for UNet tests
    - Fixed bash variable substitution bug in job_postproc scripts.
    
    - Updated all 6 UNet postprocessing configs to evaluate only on Annual data.
    
    - Configured metrics evaluation to run only the 'mean' metrics and disabled 'extremes' for a faster, lightweight evaluation.

---
+++ postproc/submit_all_postproc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sbatch job_postproc_unet_exp1.sh
+sbatch job_postproc_unet_exp2.sh
+sbatch job_postproc_unet_exp3.sh
+sbatch job_postproc_unet_exp4.sh
+sbatch job_postproc_unet_exp5.sh
+sbatch job_postproc_unet_exp6.sh
